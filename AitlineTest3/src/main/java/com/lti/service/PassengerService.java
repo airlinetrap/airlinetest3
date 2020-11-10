30020ce92a8de56adf3597f0a24770ed4f76d69e
Tandrima-Git <tandrimadutta98@gmail.com>
2020-11-10 16:04:47 +0530

    second commit

---
+++ AitlineTest3/src/main/java/com/lti/service/PassengerService.java
@@ -0,0 +1,14 @@
+package com.lti.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.lti.dto.PassengerDto;
+import com.lti.entity.Passenger;
+
+public interface PassengerService {
+	void addPassenger(Passenger passenger);
+	Passenger getPassenger(int passengerId );
+	List<Passenger> viewAllPassenger(int userId,int flightId,LocalDate dateOfTravel);
+
+}
